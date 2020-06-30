68989e7289382820bac58b60b9672c2d94dd36c2
RaspiBlitz <steinema@pm.me>
2020-06-30 10:12:58 +0100

    raspiblitz data

--- mediantime.sh
+++ mediantime.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-for HEIGHT in {0..630000}
+for HEIGHT in {0..100}
 do
     MEDIANTIME=$(bitcoin-cli getblockhash $HEIGHT | xargs bitcoin-cli getblockheader | jq .mediantime)
     echo "$HEIGHT, $MEDIANTIME"
